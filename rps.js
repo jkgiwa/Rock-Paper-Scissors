bdaffc29d12b2841959d468ba0aa0a5827c2c62e
jkgiwa <jkgiwa95@gmail.com>
2022-01-04 21:47:10 +0000

    edit rps.js

--- rps.js
+++ rps.js
@@ -8,27 +8,59 @@ function computerPlay() {
         computerChoice = "paper"}
     else { 
         computerChoice = "scissors"}
-
-    console.log(computerChoice)
+    return computerChoice
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    
-    if (playerSelection === "rock" && computerSelection === "paper") {
-         let response = "You Lose! Paper beats Rock"
-    }
-    
-    else {
-        response = "viu?"
-    }
-    
-    return response;
-    
-}
+    let response = " "
 
-const playerSelection = "rock";
+    if 
+    (playerSelection === "rock" && computerSelection === "paper") {
+        return response = "You Lose! Paper beats Rock";    //this is meant to trigger "You lose response"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        response = "You Win! Rock beats Scissors"
+    }
+
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        response = "You Win! Paper beats Rock"
+    }
+
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        response = "You Lose! Scissors beats Paper"
+    }
+    
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        response = "You Win! Scissors beats Paper"
+    }
+
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        response = "You Lose! Rock beats Scissors"
+    }
+
+    else {
+        response = "Draw -_-"
+    }
+    return response
+
+}
+    //playSelection = prompt("Rock Paper or Scissors?");
+
+
+
+const playerSelection = "paper";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
 
-        
+/*function game() {
+    let text = "Round";
+    let i = 1;
+
+    do {text += i;
+        i = i++}
+
+    while (i < 6);
+    console.log()
+}
+*/       
